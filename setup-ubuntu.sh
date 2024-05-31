728073f46ce808fb54e7f72b150022f77d165660
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2024-05-31 08:22:47 -0700

    Fix typo

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -23,7 +23,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- add tsx and treesitter
+      -- add go and treesitter
       vim.list_extend(opts.ensure_installed, {
         "go",
       })
